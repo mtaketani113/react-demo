5280fa6d057758df0bd225fe2b27360c7adf58d3
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-08-19 03:50:53 +0900

    削除と新規作成を追加

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import { Table } from 'semantic-ui-react'
+import { Table, Button, Header, Image, Modal, Form, Icon } from 'semantic-ui-react'
 
 import {
 	Container,
@@ -8,6 +8,7 @@ import {
 const Customer = () => {
 	
 	const [customers, setCustomers] = useState<any>(null);
+	const [open, setOpen] = useState<boolean>(false);
 	
 	const PRONET_CONTEXT_ENDPOINT = "http://localhost:8080/demo/";
 
@@ -21,6 +22,40 @@ const Customer = () => {
 		}).catch((reason)=>{
 			console.log(reason);
 		});
+	};
+
+	const addCustomer = () => {
+        let endpoint = PRONET_CONTEXT_ENDPOINT + "api/customer/new";
+		let name = document.querySelector<HTMLInputElement>("#customer_name")!.value;
+		let post = document.querySelector<HTMLInputElement>("#customer_post")!.value;
+		let address = document.querySelector<HTMLInputElement>("#customer_address")!.value;
+		fetch(endpoint, {cache:"no-cache", method:"PUT",
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+			},
+			body : JSON.stringify({name : name, post: post, address: address})})
+		.then( (response)=>{
+			loadCustomers()} )
+		.catch((reason)=>{
+			console.log(reason);
+		}).finally(()=>{
+			setOpen(false);
+		});
+	};
+
+	const deleteCustomer = (id: string) => {
+		if(window.confirm("削除しますか？")){
+			let endpoint = PRONET_CONTEXT_ENDPOINT + "api/customer/delete/" + id;
+			fetch(endpoint, {cache:"no-cache", method:"DELETE"})
+			.then( (response)=>{
+				loadCustomers()} )
+			.catch((reason)=>{
+				console.log(reason);
+			})
+	
+		}else{
+
+		}
 	};
 
     useEffect(() => {
@@ -39,6 +74,9 @@ const Customer = () => {
 				<Table.Cell>{customer.name}</Table.Cell>
 				<Table.Cell>{customer.post}</Table.Cell>
 				<Table.Cell>{customer.address}</Table.Cell>
+				<Table.Cell>
+					<Icon link name='trash alternate outline' onClick={() => deleteCustomer(customer.id)} />
+				</Table.Cell>
 			</Table.Row>);
 		}
         
@@ -51,12 +89,53 @@ const Customer = () => {
 							<Table.HeaderCell>名前</Table.HeaderCell>
 							<Table.HeaderCell>郵便番号</Table.HeaderCell>
 							<Table.HeaderCell>住所</Table.HeaderCell>
+							<Table.HeaderCell></Table.HeaderCell>
 						</Table.Row>
 					</Table.Header>
 					<Table.Body>
 						{rows}
 					</Table.Body>
 				</Table>
+	<Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button circular icon='plus'></Button>}
+    >
+      <Modal.Header>顧客登録</Modal.Header>
+      <Modal.Content image>
+        <Image size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' wrapped />
+        <Modal.Description>
+          <Header>登録情報</Header>
+		  <Form>
+			<Form.Field>
+				<label>名前</label>
+				<input id="customer_name" placeholder='名前' />
+			</Form.Field>
+			<Form.Field>
+				<label>郵便番号</label>
+				<input id="customer_post" placeholder='郵便番号' />
+			</Form.Field>
+			<Form.Field>
+				<label>住所</label>
+				<input id="customer_address" placeholder='住所' />
+			</Form.Field>
+		  </Form>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => setOpen(false)}>
+          閉じる
+        </Button>
+        <Button
+          content="登録"
+          labelPosition='right'
+          icon='checkmark'
+          onClick={() => addCustomer()}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
 			</Container>
 		);
 	}
