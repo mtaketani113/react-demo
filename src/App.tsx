50b7e6dcb3522a030c318d4c5ed8cda946f1b8da
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-09-09 01:38:11 +0900

    ログイン画面を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
-import {Top, Customer, Map, Files} from './components/index';
+import {Top, Customer, Map, Files, HomepageLayout} from './components/index';
 import './App.css';
 
 import {BrowserRouter, Routes , Route} from "react-router-dom";
 
 import {HeaderMenu, FooterMenu} from './components/index';
 
-import { GoogleLogin } from 'react-google-login';
 import { useCookies } from "react-cookie";
 
 import "./i18n/configs"
@@ -17,7 +16,6 @@ import * as fetchIntercept from 'fetch-intercept';
 
 function App() {
 
-  const clientId:string = process.env.REACT_APP_CLIENT_ID == null? "": process.env.REACT_APP_CLIENT_ID ;
   const [cookies, setCookie] = useCookies(["react_access_token"]);
   const [accessToken, setAccessToken] = useState<string|null>(cookies.react_access_token);
   axios.interceptors.request.use((config: AxiosRequestConfig) => {
@@ -64,19 +62,8 @@ function App() {
   return (
     <React.Fragment>
       {accessToken == null ? (
-            <GoogleLogin
-               clientId={clientId}
-               buttonText="LOGIN WITH GOOGLE"
-               onSuccess={(response) => {
-                if ('accessToken' in response) {
-                  setAccessToken(response.accessToken);
-                  setCookie("react_access_token", response.accessToken);
-                }
-                console.log(response)
-                console.log("Google Login success")}}
-               onFailure={(err) => console.log("Google Login failed", err)}
-               cookiePolicy={'single_host_origin'}
-            />
+            
+            <HomepageLayout setAccessToken={setAccessToken} setCookie={setCookie}/>
             ) : (
               <><HeaderMenu accessToken={accessToken} /><BrowserRouter>
               <Routes>
