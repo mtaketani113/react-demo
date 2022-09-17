4258ee5f0b35e597bb9f90f34355e2aa5abf75a1
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-09-18 03:47:18 +0900

    Formatterを適用

--- src/components/ModalCustomerRegister.test.tsx
+++ src/components/ModalCustomerRegister.test.tsx
@@ -3,42 +3,47 @@ import userEvent from '@testing-library/user-event';
 import ModalCustomerRegister from './ModalCustomerRegister';
 jest.mock('axios');
 
-jest.mock("react-i18next", () => ({
-	...jest.requireActual("react-i18next"),
-	useTranslation: () => {
-	  return {
-		t: (str: string) => str,
-		i18n: {
-		  language: "en",
-		  addResourceBundle: () => jest.fn(),
-		  changeLanguage: () => new Promise(() => {}),
-		},
-	  };
-	},
-  }));
-
+jest.mock('react-i18next', () => ({
+  ...jest.requireActual('react-i18next'),
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        language: 'en',
+        addResourceBundle: () => jest.fn(),
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
 
 describe('ModalCustomerRegister', () => {
-
   afterEach(cleanup);
   it('should render Modal', async () => {
     render(
-     <ModalCustomerRegister loadCustomers={function (pageNum: number): void {
+      <ModalCustomerRegister
+        loadCustomers={function (pageNum: number): void {
           throw new Error('Function not implemented.');
-        } } customer={null} pageNum={0} />
+        }}
+        customer={null}
+        pageNum={0}
+      />,
     );
     const addButton = await screen.findByTestId('addButton');
     userEvent.click(addButton);
     const inputNameFiled = await screen.findByText('customer.name');
     expect(inputNameFiled).toBeInTheDocument();
-    
   });
 
   it('should close Modal', async () => {
     render(
-     <ModalCustomerRegister loadCustomers={function (pageNum: number): void {
+      <ModalCustomerRegister
+        loadCustomers={function (pageNum: number): void {
           throw new Error('Function not implemented.');
-        } } customer={null} pageNum={0} />
+        }}
+        customer={null}
+        pageNum={0}
+      />,
     );
     const addButton = await screen.findByTestId('addButton');
     userEvent.click(addButton);
@@ -46,7 +51,5 @@ describe('ModalCustomerRegister', () => {
     userEvent.click(closeButton);
     const inputNameFiled = screen.queryByText('customer.name');
     expect(inputNameFiled).toBeNull();
-    
   });
-
 });
